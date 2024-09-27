93c46c9e05dae0ef7be0c416aeddf277a75c14d5
Santiago Gomez <Santigomez1129@gmail.com>
2024-09-26 22:31:13 -0300

    Starting the Movie finder project with vite and react

---
+++ README.md
@@ -0,0 +1,7 @@
+Movie Finder App
+
+This app will consist in a movie directory that get it's content from the https://api.themoviedb.org api.
+
+The main page will display the most popular movies starting with the first 12.
+It will have a search bar to access to the information of any movie that's in the directory.
+Then, there will be a dropdown to get movies for genre.
---
+++ src/test.js
@@ -0,0 +1,13 @@
+export const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTllMTRmMDY2ODMwNTM4YWY3ZTlhYzEyMDBlZjQxZSIsIm5iZiI6MTcyNzM5ODE1My45NTYzLCJzdWIiOiI2NmY0NzI0MzU4MjBkMjhjZmI2YTEyMTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Tkk6TDsMn7PZX_BSwHGiZ6k9rqeuuamx497P1TKRleM',
+    },
+}
+
+fetch('https://api.themoviedb.org/3/authentication', options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err))
